105ecc3e6d8d5f954e3c5dfd400faff0d5458704
Chittaranjan Panda <19926216+pandaind@users.noreply.github.com>
2022-10-04 11:48:17 +0530

    For Testing

--- src/test/java/com/example/demo/configserver/ConfigServerApplicationTests.java
+++ src/test/java/com/example/demo/configserver/ConfigServerApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.demo.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest

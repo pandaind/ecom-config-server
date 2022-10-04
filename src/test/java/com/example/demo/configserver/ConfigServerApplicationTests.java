5fb650b4c9f4448ba75959f411eb840d5fe57673
Chittaranjan Panda <19926216+pandaind@users.noreply.github.com>
2022-10-04 14:37:37 +0530

    Test Success

--- src/test/java/com/example/demo/configserver/ConfigServerApplicationTests.java
+++ src/test/java/com/example/demo/configserver/ConfigServerApplicationTests.java
@@ -2,11 +2,12 @@ package com.example.demo.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles(profiles = {"test"})
-//@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 class ConfigServerApplicationTests {
 
     @Test
